08334ad5ea3ed724314894595a1363b8baf428e1
rahath <Administrator@rahath.local>
2014-09-06 18:53:58 -0400

    Initial Commit

---
+++ SpicMacay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpicMacay
+//
+//  Created by rahath on 9/6/14.
+//  Copyright (c) 2014 Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
